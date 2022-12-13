fd0f9a3075f8adbf655f0b110c04db6863d9e5b4
Zoumana Cisse <zoumanacisse692@gmail.com>
2022-12-12 21:05:40 -0800

    first commit frontendMentor submission

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: ['./starter-code/*.html'],
+  theme: {
+    extend: {
+      colors: {
+      'dream': {
+        'blue': '#66E2DC',
+        'orange': '#FA7453',
+        'yellow': '#FFB964',
+        'white': '#FCFAF9',
+        'black': '#191826'
+
+      }
+    },
+    'fontSize' :{
+      'maintitle': ['88px',{lineHeight:'88px',letterSpacing:'-1px'}],
+      'pagetitle': ['40px',{lineHeight:'40px'}],
+      'body1':['20px',{lineHeight:'34px'}],
+      'body2':['16px',{lineHeight:'26px'}]
+    }
+    ,
+    'fontFamily':{
+      'main':['IBM Plex Sans', 'sans-serif']
+    },
+    backgroundImage: {
+      'desktop': "url('../assets/bg-main-desktop.png')",
+      'tablet': "url('../assets/bg-main-tablet.png')",
+      'mobile': "url('../assets/bg-main-mobile.png')",
+      'facebook': "url('../assets/icon-facebook.svg')"
+    },
+  },
+    screens: {
+      'xs': '375px',
+      ...defaultTheme.screens,
+      'lg': '1440px',
+      'tablet':'1024px',
+    },
+
+  },
+  plugins: [],
+}
